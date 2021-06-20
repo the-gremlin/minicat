3ed299fd1d883b96a37b0e3b25bbce44aaae071f
the_gremlin <gsfilothei@gmail.com>
2021-06-21 01:55:54 +0300

    Re-write most of the stuff here so it can be easier to work with (I hope)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use std::{env, fs, error::Error};
+use anyhow::Result;
 
 pub mod process;
 
-fn main() {
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
 
+    //having the `args[1..]` is important because env::args() has the path of
+    //the executable as the first argument
     let (file_vec, command_vec) = process::process_input(args[1..].to_vec());
 
-    println!("{:?}\n{:?}", file_vec, command_vec);
+    let massive_string = process::collect_files_to_string(file_vec)?;
+
+    println!("{}", massive_string);
+
+    Ok(())
 }
 
--- src/process.rs
+++ src/process.rs
@@ -1,4 +1,27 @@
-use std::{fs};
+use std::{
+    fs,
+    io::{self, Read},
+};
+
+use anyhow::{Result, Context};
+
+pub struct CommandFlags {
+    pub show_tabs: bool,
+    pub number_nonblanks: bool,
+    pub show_ends: bool,
+    pub numbers: bool    
+} 
+
+impl CommandFlags {
+    pub fn new() -> Self {
+        CommandFlags{
+            show_tabs: false,
+            number_nonblanks: false,
+            show_ends: false,
+            numbers: false,
+        }
+    }
+}
 
 pub fn process_input(input: Vec<String>) -> (Vec<String>, Vec<String>) {
     
@@ -23,22 +46,34 @@ pub fn process_input(input: Vec<String>) -> (Vec<String>, Vec<String>) {
     (file_vec, command_vec)
 
 }
-///The core of the "cat" command, reading files, concatenating them
-///and pushing the result to stdout
-///
-///Note that if one of these files is `-` then it will continuously read
-///from stdin and print that on stdout, without a way to exit the program
-pub fn print_files(files: Vec<String>) {
-    let master_string = String::new();
 
-    for i in files {
-        if i == String::from("-"){
-            cat_stdin();
+pub fn collect_files_to_string(file_vec: Vec<String>) -> Result<String> {
+    let files = file_vec.into_iter();
+
+    let mut master_string = String::new();
+
+    for file in files {
+        let file_contents = fs::read_to_string(&file)
+            .with_context(|| format!("Failed to read file {}", &file))?;
         
-        let file_string: String = fs::read_to_string(i);
-        master_string.push_str(&file_string);
-        }
+        master_string.push_str(file_contents.as_str());
     }
+
+    Ok(master_string)
 }
 
+pub fn process_commands(command_vec: Vec<String>) -> CommandFlags {
+    let mut flags = CommandFlags::new(); 
+
+    for i in command_vec[..] {
+        match i.as_mut_str() {
+            "T" | "-show-tabs" => flags.show_tabs = true,
+            "b" | "-number-nonblanks" => flags.number_nonblanks = true,
+            "E" | "-show_ends" => flags.show_ends = true,
+            "n" | "-numbers" => flags.numbers = true,
+            _ => panic!("invalid option -- \"{}\"", i.as_mut_str),
+        }
+    }    
+
+    flags
 }
